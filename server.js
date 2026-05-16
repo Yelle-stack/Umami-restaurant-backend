02116929a2175779a69715092ed3315ca7b3d5b2
Jelena <jelena33.bliss@gmail.com>
2026-05-16 13:58:41 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
+
+// Charger les variables d'environnement
+dotenv.config();
+
+const app = express();
+
+// PORT
+const PORT = process.env.PORT || 4000;
+
+connectDB()
+connectCloudinary()
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/user', userRouter)
+
+// Route test
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Lancer le serveur
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
