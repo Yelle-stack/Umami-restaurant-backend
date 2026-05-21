7b5218230ebf0bb20e18d0f17f3d18aa2fd959b7
Jelena <jelena33.bliss@gmail.com>
2026-05-21 19:11:39 +0200

    Add reservation routes, controllers and model

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
+import reservationRoute from './routes/reservationRoute.js'
 
 // Charger les variables d'environnement
 dotenv.config();
@@ -23,6 +24,7 @@ app.use(express.json());
 
 app.use('/api/user', userRouter)
 app.use('/api/product', productRouter)
+app.use('/api/reservations', reservationRoute)
 
 // Route test
 app.get("/", (req, res) => {
